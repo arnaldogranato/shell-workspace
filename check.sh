35284b5af8fc20e4b97688b79c8a286736675dee
arnaldogranato <arnaldogranato@gmail.com>
2025-05-08 09:56:50 -0300

    checagem de texto dos backups

---
+++ check.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# aut.txt  fbt.txt  fts.txt  h02.txt  h19.txt  h29.txt  h39.txt  ltn.txt  pal.txt  ww2.txt
+grep -i erro ../???.txt
+grep -i poss?vel ..???.txt
+grep -i problem ../???.txt
+grep -i [9-8][0-9]% ../???.txt
