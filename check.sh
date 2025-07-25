45086df513f8aa8a31665dd2f0fd8ff453a18660
arnaldogranato <arnaldogranato@gmail.com>
2025-07-25 09:43:07 -0300

    melhorias no check

--- check.sh
+++ check.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+bbash
 
 # aut.txt  fbt.txt  fts.txt  h02.txt  h19.txt  h29.txt  h39.txt  ltn.txt  pal.txt  ww2.txt
-grep -i erro ../???.txt
-grep -i poss?vel ..???.txt
-grep -i problem ../???.txt
-grep -i [9-8][0-9]% ../???.txt
+ls -la old/bkp-base/???.txt | wc -l; echo = ;ls -la ???.txt | wc -l
+grep -i erro ???.txt | grep -v tour.ERRO[A-Z][A-Z][A-Z]
+grep -i poss?vel ???.txt
+grep -i problem ???.txt | grep -v caso
+grep -i mont ???.txt | grep -v alend
+grep [8-9][0-9]% ???.txt
+grep 100% ???.txt
